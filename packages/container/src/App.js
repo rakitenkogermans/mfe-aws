ea5018fc2f20f2a6aa9fbe9b114b61e7e39f4b3e
rakitenkogermans <rakitenko.germans@gmail.com>
2024-01-04 17:58:21 +0200

    init

---
+++ packages/container/src/App.js
@@ -0,0 +1,15 @@
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return (
+      <div>
+        <h1>
+          hiiiii
+        </h1>
+        <hr/>
+        <MarketingApp />
+      </div>
+  );
+};
+
+import React from 'react';
