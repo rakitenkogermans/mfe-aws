b20b824fdfcae488683795d93a467daf2c533b10
rakitenkogermans <rakitenko.germans@gmail.com>
2024-01-08 12:54:40 +0200

    added generateClassName

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -4,11 +4,16 @@ import { StylesProvider } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
+import {createGenerateClassName} from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+})
 
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
